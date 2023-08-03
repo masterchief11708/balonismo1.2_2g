eb1dd0a215ddab7c61138c4f52d31555423b9315
Leonardo Cavalcante <rm87155@estudante.fieb.edu.br>
2023-08-03 10:36:53 -0300

    balonismo2g

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        // construindo balões
+        Balão b1 = new Balão();
 
-        // Construindo balões
+        Balão b2 = new Balão(10, "amarelo", "grande", 1.43, 23.65, "grande", 4 );
+        Balão b3 = new Balão(12, "verde", "pequeno", 1.21, 78.92, "medio", 12);
 
-        Balao b1 = new Balao();
+        // construindo pessoa
+        Pessoa p1 = new Pessoa("Leonardo", "7438328", 795345, 55119433, 551193432, "rua. arco-iris");
+        Pessoa p2 = new Pessoa("Ana", "7653456", 439023, 55110349, 551190549, "rua. gramado");
 
-        // Chamando o método voar ()
+        // construindo piloto
+        Piloto pi1 = new Piloto("Diego", "743829", 842394);
 
-        String resultado = b1.voar(new Combustivel());
-
-        // Exibindo resultado do voo
-        System.out.println(resultado);
     }
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,6 +1,41 @@
 public class Piloto {
 
+
+    //Atributos
     String nome;
-    int codAnac;
-    int cpf;
+    String CPF;
+    int num_ANAC;
+
+    public Piloto(String nome, String CPF, int num_ANAC) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.num_ANAC = num_ANAC;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public int getNum_ANAC() {
+        return num_ANAC;
+    }
+
+    public void setNum_ANAC(int num_ANAC) {
+        this.num_ANAC = num_ANAC;
+    }
+
+    //Métodos
+    public void pilotar() {System.out.println("Voar");}
 }
